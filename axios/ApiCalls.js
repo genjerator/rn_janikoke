b20fb465d5143f081ed10b5a254c5525e0b785ff
Genjerator <e.medjesi@gmail.com>
2024-04-23 20:31:48 +0200

    new source 1

--- axios/ApiCalls.js
+++ axios/ApiCalls.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {API_URL} from "../Constants";
 
 export const fetchChallengesData = async () => {
-    const url = API_URL + '/round/8';
+    const url = API_URL + '/round/1';
     try {
         const response = await axios.get(url);
         return response.data.data;
@@ -13,7 +13,7 @@ export const fetchChallengesData = async () => {
 
 export const postInsidePolygon = async (payload) => {
     try {
-        const url =API_URL + '/round/inside/8';
+        const url =API_URL + '/round/inside/1';
         console.log(url,":url");
         const response = await axios.post(url, payload);
         console.log(payload,"payload");
